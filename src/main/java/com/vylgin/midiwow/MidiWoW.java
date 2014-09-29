0a7cc68c5ad0976f57a2d72071ed73ba4d751200
Vitaliy Vylgin <vitaliy_vylgin@etb-lab.com>
2014-09-29 12:10:31 +0400

    Added system style for OS X and Nimbus style for other OS.

--- src/main/java/com/vylgin/midiwow/MidiWoW.java
+++ src/main/java/com/vylgin/midiwow/MidiWoW.java
@@ -1,10 +1,11 @@
 package com.vylgin.midiwow;
 
-import javax.swing.*;
-
+import com.vylgin.midiwow.helper.OsCheck;
 import com.vylgin.midiwow.ui.MainWindow;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.swing.*;
 
 /**
  * Main class for running program
@@ -15,14 +16,34 @@ public class MidiWoW {
     
     public static void main(String[] args) {
         log.info("MidiWoW starting.");
-        System.setProperty("apple.laf.useScreenMenuBar", "true");
-        try {
-            System.setProperty("apple.laf.useScreenMenuBar", "true");    
-            System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Test");
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException e) {
-            log.error("Exception!", e);
+
+        OsCheck.OSType ostype= OsCheck.getOperatingSystemType();
+        switch (ostype) {
+            case MacOS:
+                try {
+                    System.setProperty("apple.laf.useScreenMenuBar", "true");
+                    System.setProperty("apple.laf.useScreenMenuBar", "true");
+                    System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Test");
+                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException e) {
+                    log.error("Exception!", e);
+                }
+                break;
+            case Windows:
+                try {
+                        for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                                if ("Nimbus".equals(info.getName())) {
+                                        javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                                        break;
+                                }
+                        }
+                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException e) {
+                        log.error("Exception!", e);
+                }
+            case Linux:
+            case Other: break;
         }
+
         
         MidiWoW midiWoW = new MidiWoW();
         midiWoW.go();
